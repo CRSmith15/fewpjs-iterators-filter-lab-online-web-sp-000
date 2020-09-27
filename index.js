c86c0c1d6fd56275f015eca5e5b67e3d26e4af82
Chance Smith <chancesmith97@hotmail.com>
2020-09-27 20:50:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function findMatching(drivers, str) {
 }
 
 function fuzzyMatch(drivers, str){
-  return drivers.filter(n => n.charAt(0) == str.charAt(0))
+  return drivers.filter(n => n.charAt(0) == str.charAt(0));
 }
