928bac55f90fc274cbe694da7a858caea22a3235
Chance Smith <chancesmith97@hotmail.com>
2020-09-27 20:49:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(drivers, str) {
   return drivers.filter(n => n.toUpperCase() == str.toUpperCase());
 }
+
+function fuzzyMatch(drivers, str){
+  return drivers.filter(n => n.charAt(0) == str.charAt(0))
+}
